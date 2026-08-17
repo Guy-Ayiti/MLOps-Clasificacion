1a3def3e82884afbfa77de4968fe8217b78a4b02
Guy-Ayiti <guy3hil@hotmail.com>
2026-08-17 14:29:32 -0600

    remove dataset creation in tf

--- Terraform_Vertex/main.tf
+++ Terraform_Vertex/main.tf
@@ -4,87 +4,13 @@ provider "google" {
 }
 
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = var.dataset_id_1
-  project    = var.project_id
-  location   = var.region
-}
- 
 
-resource "google_bigquery_table" "tableTrain" {
-  table_id   = var.table_id_1
-  dataset_id = google_bigquery_dataset.dataset.dataset_id
-  project    = var.project_id
-
-  schema = jsonencode([
-    {
-      name = "SepalLength"
-      type = "FLOAT"
-      mode = "NULLABLE"
-    },
-    {
-      name = "SepalWidth"
-      type = "FLOAT"
-      mode = "NULLABLE"
-    },
-    {
-      name = "PetalLength"
-      type = "FLOAT"
-      mode = "NULLABLE"
-    },
-    {
-      name = "PetalWidth"
-      type = "FLOAT"
-      mode = "NULLABLE"
-    },
-    {
-      name = "Species"
-      type = "STRING"
-      mode = "NULLABLE"
-    }
-  ])
-}
-
-
-resource "google_bigquery_table" "tableTest" {
-  table_id   = var.table_id_2
-  dataset_id = google_bigquery_dataset.dataset.dataset_id
-  project    = var.project_id
-
-  schema = jsonencode([
-    {
-      name = "SepalLength"
-      type = "FLOAT"
-      mode = "NULLABLE"
-    },
-    {
-      name = "SepalWidth"
-      type = "FLOAT"
-      mode = "NULLABLE"
-    },
-    {
-      name = "PetalLength"
-      type = "FLOAT"
-      mode = "NULLABLE"
-    },
-    {
-      name = "PetalWidth"
-      type = "FLOAT"
-      mode = "NULLABLE"
-    },
-    {
-      name = "Species"
-      type = "STRING"
-      mode = "NULLABLE"
-    }
-  ])
-}
 
 
 resource "google_bigquery_table_iam_binding" "tableTrain_access_viewer" {
-  table_id   = google_bigquery_table.tableTrain.table_id
-  dataset_id = google_bigquery_table.tableTrain.dataset_id
-  project    = google_bigquery_table.tableTrain.project
+  table_id   = iris-train        #google_bigquery_table.tableTrain.table_id
+  dataset_id = BaseDatosIris      #google_bigquery_table.tableTrain.dataset_id
+  project    = proyecto-fuente  #google_bigquery_table.tableTrain.project
 
   role    = "roles/bigquery.dataViewer"
   members = [
@@ -94,9 +20,9 @@ resource "google_bigquery_table_iam_binding" "tableTrain_access_viewer" {
 
 
 resource "google_bigquery_table_iam_binding" "tableTest_access_viewer" {
-  table_id   = google_bigquery_table.tableTest.table_id
-  dataset_id = google_bigquery_table.tableTest.dataset_id
-  project    = google_bigquery_table.tableTest.project
+  table_id   = iris-test   #google_bigquery_table.tableTest.table_id
+  dataset_id = BaseDatosIris  #google_bigquery_table.tableTest.dataset_id
+  project    = proyecto-fuente   #google_bigquery_table.tableTest.project
 
   role    = "roles/bigquery.dataViewer"
   members = [
